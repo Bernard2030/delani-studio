b3ed6ae0becb516b9bba6370ccd2d1a0f72d0f84
Bernard2030 <bernard.mukhenye@student.moringa.com>
2021-07-23 23:20:38 +0300

    add js content

---
+++ All-view/js/scripts.js
@@ -0,0 +1,9 @@
+// user interface
+
+$(document).ready(function(delaniMessage){
+    $("#click").click(function(){
+        $("#onCheck1").toggle();
+        $("#onCheck2").toggle();
+        $("#onCheck3").toggle();
+    })
+})
